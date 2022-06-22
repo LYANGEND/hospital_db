0b0eee3c430c2f7f2ba25566e795fedc5917a7e8
lyamz <davidlyangenda@gmail.com>
2022-06-22 18:52:45 +0200

    create schema.sql

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,46 @@
+CREATE DATABSE hospital_ds
+\C hospital_ds
+
+CREATE TABLE medical_histories (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+admitted_at timestamp, 
+patient_id INT,
+status VARCHAR (50)
+CONSTRAINT patient_id 
+FOREIGN KEY(patient_id)
+REFERENCES patient (id)
+);
+
+CREATE TABLE patient (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name VARCHAR (100),
+date_of_birth date
+);
+
+CREATE TABLE treatment (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+type VARCHAR (100),
+name VARCHAR (100)
+);
+
+CREATE TABLE invoice (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+total_amount decimal,
+generate_at timestamp,
+paid_at timestamp,
+medical_histore_id INT,
+CONSTRAINT medical_histore_id
+FOREIGN KEY(medical_histore_id)
+REFERENCES medical_histories (id)
+);
+
+CREATE TABLE invoice_item (
+id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT,
+CONSTRAINT invoice_id 
+FOREIGN KEY(invoice_id)
+REFERENCES invoice (id)
+);
