bd65cdfd9a4ff3c31d8075fe5968a259c3dcffb4
lyamz <davidlyangenda@gmail.com>
2022-06-23 11:52:47 +0200

    update requested changes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,51 +1,44 @@
 CREATE DATABSE hospital_ds
 \C hospital_ds
 
-CREATE TABLE medical_histories (
-id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-admitted_at timestamp, 
-patient_id INT,
-status VARCHAR (50)
-CONSTRAINT patient_id 
-FOREIGN KEY(patient_id)
-REFERENCES patient (id)
+CREATE TABLE patients(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(20),
+  date_of_birth date,
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE patient (
-id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-name VARCHAR (100),
-date_of_birth date
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES  medical_histories(id),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE treatment (
-id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-type VARCHAR (100),
-name VARCHAR (100)
+CREATE TABLE invoice_items(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT REFERENCES medical_historeis(id),
+  treatment_id INT REFERENCES treatment(id),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE invoice (
-id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-total_amount decimal,
-generate_at timestamp,
-paid_at timestamp,
-medical_histore_id INT,
-CONSTRAINT medical_histore_id
-FOREIGN KEY(medical_histore_id)
-REFERENCES medical_histories (id)
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  pattient_id INT REFERENCES pattient(id),
+  status VARCHAR,
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE invoice_item (
-id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-unit_price DECIMAL,
-quantity INT,
-total_price DECIMAL,
-invoice_id INT,
-CONSTRAINT invoice_id 
-FOREIGN KEY(invoice_id)
-REFERENCES invoice (id)
-);
-
-CREATE TABLE medical_histories_has_treatments (
- medical_history_id int refrences medical_histories(id),
- treatment_id int refrences treatments(id),
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR,
+  name VARCHAR,
+  PRIMARY KEY(id)
 );
